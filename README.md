1fa3b5d069c96d9b4a5caae6a839c0f0546d69e1
coolmarvel <marvel97@naver.com>
2022-01-11 09:13:37 +0900

    mimicoin

---
+++ README.md
@@ -0,0 +1,3 @@
+# mimicoin
+
+blockchain_project
---
+++ src/main.js
@@ -0,0 +1,45 @@
+const { Blockchain, Transaction } = require("./blockchain");
+const EC = require("elliptic").ec;
+const ec = new EC("secp256k1");
+
+// Your private key goes here
+const myKey = ec.keyFromPrivate(
+  "4da73bae30009e8284e2c1ee42ff77c22a99fe03b8c36937f4471964efc2f921"
+);
+
+// From that we can calculate your public key (which doubles as your wallet address)
+const myWalletAddress = myKey.getPublic("hex");
+
+// Create new instance of Blockchain class
+const mimiCoin = new Blockchain();
+
+// Mine first block
+mimiCoin.minePendingTransactions(myWalletAddress);
+
+// Create a transaction & sign it with your key
+const tx1 = new Transaction(myWalletAddress, "address2", 100);
+tx1.signTransaction(myKey);
+mimiCoin.addTransaction(tx1);
+
+// Mine block
+mimiCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx2 = new Transaction(myWalletAddress, "address1", 50);
+tx2.signTransaction(myKey);
+mimiCoin.addTransaction(tx2);
+
+// Mine block
+mimiCoin.minePendingTransactions(myWalletAddress);
+
+console.log();
+console.log(
+  `Balance of marvel is ${mimiCoin.getBalanceOfAddress(myWalletAddress)}`
+);
+
+// Uncomment this line if you want to test tampering with the chain
+// mimiCoin.chain[1].transactions[0].amount = 10;
+
+// Check if the chain is valid
+console.log();
+console.log("Blockchain valid?", mimiCoin.isChainValid() ? "Yes" : "No");
